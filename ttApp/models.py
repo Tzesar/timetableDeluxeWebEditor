f33b0f8a8c2bd149b2237a5b32624f027c13855e
Augusto <agu.amarilla@gmail.com>
2014-03-03 21:01:37 -0300

    It can edit all the data. The problem is that it displays the time data as integers.

--- ttApp/models.py
+++ ttApp/models.py
@@ -1,15 +1,30 @@
 from django.db import models
+import string
+import random
+
+def id_generator(size=32, chars=string.ascii_lowercase + string.digits):
+    """
+
+    :param size: integer, indicates the size of the id
+    :param chars: strings, the samples to randomly select from
+    :return: string randomly generated from chars with size length
+    """
+    return ''.join(random.choice(chars) for _ in range(size))
 
 # Create your models here.
 # GLOBAL_ID format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
 
 class TIMETABLE(models.Model):
     _id = models.AutoField(primary_key=True)
-    GLOBAL_ID = models.CharField(max_length=29)
     NAME = models.CharField(max_length=30)
-    VALID_FROM = models.IntegerField()
-    VALID_UNTIL = models.IntegerField()
-    DEFAULT_LENGTH = models.IntegerField()
+    GLOBAL_ID = models.CharField(max_length=36)
+    VALID_FROM = models.IntegerField(blank=True)
+    VALID_UNTIL = models.IntegerField(blank=True)
+    DEFAULT_LENGTH = models.IntegerField(default=45)
+
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(TIMETABLE, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.NAME
@@ -21,7 +36,11 @@ class TEACHER(models.Model):
     _id = models.AutoField(primary_key=True)
     GLOBAL_ID = models.CharField(max_length=36)
     NAME = models.CharField(max_length=30)
-    MAIL = models.CharField(max_length=30)
+    MAIL = models.CharField(max_length=30, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(TEACHER, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.NAME
@@ -31,32 +50,40 @@ class TEACHER(models.Model):
 
 class SUBJECT(models.Model):
     _id = models.AutoField(primary_key=True)
+    NAME = models.CharField(max_length=35)
     GLOBAL_ID = models.CharField(max_length=36)
-    NAME = models.CharField(max_length=30)
     SHORT_NAME = models.CharField(max_length=5)
     COLOR = models.IntegerField()
-    TEACHER_ID = models.ForeignKey(TEACHER)
+    TEACHER_ID = models.ForeignKey(TEACHER, db_column='TEACHER_ID')
     URL = models.CharField(max_length=30, blank=True)
     NOTE = models.CharField(max_length=30, blank=True)
 
     def __unicode__(self):
         return self.NAME
 
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(SUBJECT, self).save(*args, **kwargs)
+
     class Meta:
         db_table = 'SUBJECT'
 
 class TASK(models.Model):
     _id = models.AutoField(primary_key=True)
-    GLOBAL_ID = models.CharField(max_length=29)
+    GLOBAL_ID = models.CharField(max_length=36)
     NAME = models.CharField(max_length=30)
     IS_DONE = models.IntegerField()
     DESCRIPTION = models.TextField()
-    SUBJECT_ID = models.ForeignKey(SUBJECT)
+    SUBJECT_ID = models.ForeignKey(SUBJECT, db_column='SUBJECT_ID')
     PRIORITY = models.IntegerField()
     DUE_UNTIL = models.IntegerField()
 
     def __unicode__(self):
         return self.NAME
+
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(TASK, self).save(*args, **kwargs)
 
     class Meta:
         db_table = 'TASK'
@@ -70,6 +97,10 @@ class HOLIDAY(models.Model):
 
     def __unicode__(self):
         return self.NAME
+
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(HOLIDAY, self).save(*args, **kwargs)
 
     class Meta:
         db_table = 'HOLIDAY'
@@ -98,6 +129,10 @@ class LESSON_TYPE(models.Model):
     def __unicode__(self):
         return self.NAME
 
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(LESSON_TYPE, self).save(*args, **kwargs)
+
     class Meta:
         db_table = 'LESSON_TYPE'
 
@@ -109,15 +144,19 @@ class LOCATION(models.Model):
     def __unicode__(self):
         return self.NAME
 
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(LOCATION, self).save(*args, **kwargs)
+
     class Meta:
         db_table = 'LOCATION'
 
 class LESSON(models.Model):
     _id = models.AutoField(primary_key=True)
     GLOBAL_ID = models.CharField(max_length=36)
-    NAME = models.CharField(max_length=30)
-    TIMETABLE_ID = models.ForeignKey(TIMETABLE)
-    SUBJECT_ID = models.ForeignKey(SUBJECT)
+    NAME = models.CharField(max_length=30, default='unknown')
+    TIMETABLE_ID = models.ForeignKey(TIMETABLE, db_column='TIMETABLE_ID')
+    SUBJECT_ID = models.ForeignKey(SUBJECT, db_column='SUBJECT_ID')
     DAY_ID = models.IntegerField()
     # START_TIME and END_TIME measured in seconds from 00:00 to 23:59
     START_TIME = models.IntegerField()
@@ -126,12 +165,16 @@ class LESSON(models.Model):
     VALID_TO = models.IntegerField()
     WEEK_ID = models.IntegerField()
     NOTE = models.CharField(max_length=30)
-    TEACHER_ID = models.ForeignKey(TEACHER)
-    LOCATION_ID = models.ForeignKey(LOCATION)
-    TYPE_ID = models.ForeignKey(LESSON_TYPE)
+    TEACHER_ID = models.ForeignKey(TEACHER, db_column='TEACHER_ID')
+    LOCATION_ID = models.ForeignKey(LOCATION, db_column='LOCATION_ID')
+    TYPE_ID = models.ForeignKey(LESSON_TYPE, db_column='TYPE_ID')
 
     def __unicode__(self):
         return self.NAME
+
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(LESSON, self).save(*args, **kwargs)
 
     class Meta:
         db_table = 'LESSON'
@@ -144,6 +187,10 @@ class EXAM_TYPE(models.Model):
     def __unicode__(self):
         return self.NAME
 
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(EXAM_TYPE, self).save(*args, **kwargs)
+
     class Meta:
         db_table = 'EXAM_TYPE'
 
@@ -155,6 +202,10 @@ class EXAM_GROUP(models.Model):
     def __unicode__(self):
         return self.NAME
 
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(EXAM_GROUP, self).save(*args, **kwargs)
+
     class Meta:
         db_table = 'EXAM_GROUP'
 
@@ -162,18 +213,22 @@ class EXAM(models.Model):
     _id = models.AutoField(primary_key=True)
     GLOBAL_ID = models.CharField(max_length=36)
     TIME_MILLIS = models.IntegerField()
-    SUBJECT_ID = models.ForeignKey(SUBJECT)
-    TIMETABLE_ID = models.ForeignKey(TIMETABLE)
+    SUBJECT_ID = models.ForeignKey(SUBJECT, db_column='SUBJECT_ID')
+    TIMETABLE_ID = models.ForeignKey(TIMETABLE, db_column='TIMETABLE_ID')
     NAME = models.CharField(max_length=30)
     GRADE = models.FloatField()
     WEIGHT = models.FloatField()
     NOTE = models.CharField(max_length=30)
     PASSED = models.IntegerField()
-    TYPE_ID = models.ForeignKey(EXAM_TYPE)
-    GROUP_ID = models.ForeignKey(EXAM_GROUP)
+    TYPE_ID = models.ForeignKey(EXAM_TYPE, db_column='TYPE_ID')
+    GROUP_ID = models.ForeignKey(EXAM_GROUP, db_column='GROUP_ID')
 
     def __unicode__(self):
         return self.NAME
+
+    def save(self, *args, **kwargs):
+        self.GLOBAL_ID = id_generator()
+        super(EXAM, self).save(*args, **kwargs)
 
     class Meta:
         db_table = 'EXAM'
