9793dc42024b0dafb29b7db37e96cedf76875e32
Augusto <agu.amarilla@gmail.com>
2014-03-03 21:01:37 -0300

    Changed the password and username for the administration page.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ This is a project to create a web-based editor of schedules managed by Schedule 
 Schedule Deluxe can be downloaded from https://play.google.com/store/apps/details?id=tobi.tools.timetable
 Sadly I had nothing to do with the development of that great app, all the credits belong to Tobias Schurg.
 
+User:user
+Password:user
+
--- ttApp/admin.py
+++ ttApp/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from ttApp.models import *
 
+class ttAppAdmin(admin.ModelAdmin):
+    exclude = ('GLOBAL_ID',)
+
 # Register your models here.
-admin.site.register(TIMETABLE)
-admin.site.register(TASK)
-admin.site.register(HOLIDAY)
-admin.site.register(SUBJECT)
-admin.site.register(LESSON)
-admin.site.register(TEACHER)
-admin.site.register(LOCATION)
-admin.site.register(LESSON_TYPE)
-admin.site.register(EXAM)
-admin.site.register(EXAM_TYPE)
-admin.site.register(EXAM_GROUP)
+admin.site.register(TIMETABLE, ttAppAdmin)
+admin.site.register(TASK, ttAppAdmin)
+admin.site.register(HOLIDAY, ttAppAdmin)
+admin.site.register(SUBJECT, ttAppAdmin)
+admin.site.register(LESSON, ttAppAdmin)
+admin.site.register(TEACHER, ttAppAdmin)
+admin.site.register(LOCATION, ttAppAdmin)
+admin.site.register(LESSON_TYPE, ttAppAdmin)
+admin.site.register(EXAM, ttAppAdmin)
+admin.site.register(EXAM_TYPE, ttAppAdmin)
+admin.site.register(EXAM_GROUP, ttAppAdmin)
 
